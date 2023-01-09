9fc9c582f8e1ee5d25ea873a9744d6a69fce839a
Thiri <thirihtet246@gmail.com>
2023-01-09 23:05:56 +0630

    Add home page
    Added html and add, remove, show functions for home page

---
+++ js/app.js
@@ -0,0 +1,59 @@
+const form = document.querySelector('form');
+const bookList = document.querySelector('.book-list');
+const titleInput = document.querySelector('#titleInput');
+const authorInput = document.querySelector('#authorInput');
+const addBtn = document.querySelector('.add-btn');
+
+let collections = JSON.parse(localStorage.getItem('collections')) || [];
+
+function addBook() {
+  collections.push({
+    title: titleInput.value,
+    author: authorInput.value,
+  });
+  localStorage.setItem('collections', JSON.stringify(collections));
+}
+
+function showBook() {
+  bookList.innerHTML = '';
+  collections.forEach((collection, index) => {
+    bookList.innerHTML += `<div class='book${index + 1}' id="${index}">
+            <p class="book-info">
+                <span class="title">${collection.title}</span>
+                <br/>
+                <span class="author">${collection.author}</span>
+            </p>
+            <button class="remove-btn">Remove</button>
+            <hr/>
+    </div>`;
+  });
+}
+
+function removeBook(cur) {
+  cur.parentElement.remove();
+  collections = collections.filter((collection, index) => index !== Number(cur.parentElement.id));
+  localStorage.setItem('collections', JSON.stringify(collections));
+}
+
+showBook();
+
+form.addEventListener('submit', (e) => {
+  if (titleInput.value !== '' && authorInput.value !== '') {
+    addBtn.disabled = false;
+    addBook();
+    titleInput.value = '';
+    authorInput.value = '';
+    showBook();
+
+    e.preventDefault();
+  } else {
+    addBtn.disabled = true;
+  }
+});
+
+bookList.addEventListener('click', (e) => {
+  if (e.target.className.includes('remove-btn')) {
+    const targetEl = e.target;
+    removeBook(targetEl);
+  }
+});
