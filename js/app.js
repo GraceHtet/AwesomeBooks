c4b68d06bfa04deeee690efcc5b2e628d864634d
TamaraDimov <tamemojsoska@gmail.com>
2023-01-10 22:57:38 +0100

    create webpage according to the requirements for day 3

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
-const form = document.querySelector('form');
-const bookList = document.querySelector('.book-list');
-const titleInput = document.querySelector('#titleInput');
-const authorInput = document.querySelector('#authorInput');
-const addBtn = document.querySelector('.add-btn');
+const form = document.querySelector("form");
+const bookList = document.querySelector(".book-list");
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const addBtn = document.querySelector(".add-btn");
 
 class BookFun {
   constructor() {
-    this.collections = JSON.parse(localStorage.getItem('collections')) || [];
+    this.collections = JSON.parse(localStorage.getItem("collections")) || [];
   }
 
   removeBook(cur) {
     cur.parentElement.remove();
     this.collections = this.collections.filter(
-      (collection, index) => index !== Number(cur.parentElement.id),
+      (collection, index) => index !== Number(cur.parentElement.id)
     );
 
-    localStorage.setItem('collections', JSON.stringify(this.collections));
+    localStorage.setItem("collections", JSON.stringify(this.collections));
   }
 
   showBook() {
-    bookList.innerHTML = '';
+    bookList.innerHTML = "";
     this.collections.forEach((collection, index) => {
       bookList.innerHTML += `<div id="${index}" class='books'>
               <p class="book-info">
@@ -34,30 +34,80 @@ class BookFun {
 
   addBook(book) {
     this.collections.push(book);
-    localStorage.setItem('collections', JSON.stringify(this.collections));
+    localStorage.setItem("collections", JSON.stringify(this.collections));
   }
 }
 const bookFun = new BookFun();
 bookFun.showBook();
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const book = { title: titleInput.value, author: authorInput.value };
 
-  if (titleInput.value !== '' && authorInput.value !== '') {
+  if (titleInput.value !== "" && authorInput.value !== "") {
     addBtn.disabled = false;
     bookFun.addBook(book);
-    titleInput.value = '';
-    authorInput.value = '';
+    titleInput.value = "";
+    authorInput.value = "";
     bookFun.showBook(book);
   } else {
     addBtn.disabled = true;
   }
 });
 
-bookList.addEventListener('click', (e) => {
-  if (e.target.className.includes('remove-btn')) {
+bookList.addEventListener("click", (e) => {
+  if (e.target.className.includes("remove-btn")) {
     const targetEl = e.target;
     bookFun.removeBook(targetEl);
   }
 });
+
+// Date and time
+const clockElement = document.getElementById("clock");
+
+function clock() {
+  clockElement.textContent = new Date().toString();
+}
+
+setInterval(clock, 1000);
+
+// nav bar
+/* eslint-disable no-unused-vars */
+const displaySection = function displaySection(section) {
+  const sectionP = document.getElementById("description");
+  const heading = document.getElementById("title");
+  const sectionList = document.getElementById("list");
+  const sectionForm = document.getElementById("form");
+  const sectionContact = document.getElementById("contact");
+
+  switch (section) {
+    case "list":
+      heading.innerHTML = "All Awesome Books";
+      sectionList.style.display = "block";
+      sectionForm.style.display = "none";
+      sectionContact.style.display = "none";
+      sectionP.style.display = "none";
+      break;
+
+    case "form":
+      heading.innerHTML = "Add a New Book";
+      sectionList.style.display = "none";
+      sectionForm.style.display = "block";
+      sectionContact.style.display = "none";
+      sectionP.style.display = "none";
+      break;
+
+    case "contact":
+      heading.innerHTML = "Contact Information";
+      sectionList.style.display = "none";
+      sectionForm.style.display = "none";
+      sectionContact.style.display = "block";
+      sectionP.style.display = "none";
+      break;
+
+    default:
+      break;
+  }
+};
+/* eslint-disable no-unused-vars */
+//used becouse of error-> 'displaySection' is assigned a value but never used
